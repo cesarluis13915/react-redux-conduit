3d15b22fcd69ab0a142213e8d6882be6cfe334a6
Cesar Luis <cesarluis13915@gmail.com>
2018-10-25 22:58:46 -0600

    The principles of redux: State, reducer, action.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  
+  componentWillMount() {
+    this.store = this.props.store;
+    this.store.subscribe(() => this.setState(this.store.getState()));
+  }
+
   render() {
+    const onClick = () => this.store.dispatch({ type: 'TOGGLE' });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello, World!</h1>
+        <div>
+          Learn Redux
+          <input
+            type="checkbox"
+            checked={!!this.state.checked}
+            onClick={onClick}/>
+        </div>
+        {
+          this.state.checked ? (<h2>Done!</h2>) : null
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+  appName: 'conduit',
+  articles: null
+};
+
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <App store={store}/>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
