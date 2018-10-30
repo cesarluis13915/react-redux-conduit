8f637dec30eab09643ab16291d599ee4b92412b0
cesarluis <cesarluis13915@gmail.com>
2018-10-29 19:28:41 -0600

    Setup of react-redux library completed.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
+const mapStateToProps = state => ({
+  appName: state.appName
+});
+
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-  
-  componentWillMount() {
-    console.log('Props:', this.props);
-    this.store = this.props.store;
-    this.store.subscribe(() => this.setState(this.store.getState()));
-  }
-
   render() {
-    const onClick = () => this.store.dispatch({ type: 'TOGGLE' });
-
     return (
       <div>
-        <h1>Hello, World!</h1>
-        <div>
-          Learn Redux
-          <input
-            type="checkbox"
-            checked={!!this.state.checked}
-            onClick={onClick}/>
-        </div>
-        {
-          this.state.checked ? (<h2>Done!</h2>) : null
-        }
+        { this.props.appName }
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, () => {})(App);
